539c477b46ac4a0c483ec642de78d4efdf327e7e
Said Ali <said@babito.co.uk>
2016-04-09 19:59:32 +0100

    update init file

--- currencylayer/__init__.py
+++ currencylayer/__init__.py
@@ -1,5 +1,6 @@
-"""currencylayer - currencylayer  python API client[D[D[A"""
+"""currencylayer - currencylayer  python API client"""
 
 __version__ = '0.1'
 __author__ = 'Said Ali <said.ali@msn.com>'
 __all__ = []
+from currencylayer.client import Client
