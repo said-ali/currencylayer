6104326457f537805075db754d89b33209739cd1
Said Ali <said@babito.co.uk>
2016-04-10 11:26:08 +0100

    Return decimal for rates

--- currencylayer/client.py
+++ currencylayer/client.py
@@ -1,4 +1,5 @@
 import datetime
+import decimal
 import requests
 
 
@@ -19,27 +20,27 @@ class Client(object):
 
     def currencies(self):
         response = self.client.get(self.endpoint_currencies)
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
 
     def live_rates(self, base_currency='USD'):
         if isinstance(base_currency, list):
             base_currency = ','.join(base_currency)
         response = self.client.get(self.endpoint_live, params={'source': base_currency})
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
 
     def live_rates_for(self, currencies, base_currency='USD'):
         currencies = ','.join(currencies)
         response = self.client.get(self.endpoint_live, params={'currencies': currencies, 'source': base_currency})
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
 
     def historical(self, date=datetime.date.today(), base_currency='USD'):
         response = self.client.get(self.endpoint_historical, params={'date': date, 'source': base_currency})
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
 
     def convert(self, from_currency, to_currency, amount, date=datetime.date.today()):
         response = self.client.get(self.endpoint_convert,
                                    params={'from': from_currency, 'to': to_currency, 'amount': amount, 'date': date})
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
 
     def timeframe(self, start_date, end_date, currencies, base_currency='USD'):
         if isinstance(currencies, list):
@@ -55,4 +56,4 @@ class Client(object):
         response = self.client.get(self.endpoint_change,
                                    params={'currencies': currencies, 'start_date': start_date, 'end_date': end_date,
                                            'source': base_currency})
-        return response.json()
+        return response.json(parse_int=decimal.Decimal, parse_float=decimal.Decimal)
